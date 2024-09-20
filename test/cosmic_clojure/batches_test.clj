dc9ad91699bf2e43a46d60e838ddef45740d5e4a
Hoon Wee <mrgravity817@gmail.com>
2024-09-20 23:28:14 +0900

    refactor: use malli to generate batch and order line in tests

--- test/cosmic_clojure/batches_test.clj
+++ test/cosmic_clojure/batches_test.clj
@@ -1,35 +1,48 @@
 (ns cosmic-clojure.batches-test
   (:require
    [clojure.test :refer [deftest is testing]]
-   [cosmic-clojure.batches :as batches]))
+   [cosmic-clojure.batches :as batches :refer [Batch OrderLine]]
+   [malli.generator :as mg]))
 
 ;; Test helpers
 
 (defn- now [] (new java.util.Date))
 
+(defn- ->batch
+  [& {:keys [reference sku quantity eta]}]
+  (cond-> (mg/generate Batch)
+    (some? reference) (assoc :Batch/reference reference)
+    (some? sku) (assoc :Batch/sku sku)
+    (some? quantity) (assoc :Batch/quantity quantity)
+    (some? eta) (assoc :Batch/eta eta)))
+
+(defn- ->order-line
+  [& {:keys [order-id sku quantity]}]
+  (cond-> (mg/generate OrderLine)
+    (some? order-id) (assoc :OrderLine/order-id order-id)
+    (some? sku) (assoc :OrderLine/sku sku)
+    (some? quantity) (assoc :OrderLine/quantity quantity)))
+
 (defn- ->batch-and-line
   "Test helper function that creates a batch and order line from given input."
   [{:keys [sku batch-qty line-qty]}]
-  [#:Batch{:reference "batch-001"
-           :sku       sku
-           :quantity  batch-qty
-           :eta       (now)}
-   #:OrderLine{:order-id "order-123"
-               :sku      sku
-               :quantity line-qty}])
+  [(->batch {:sku      sku
+             :quantity batch-qty})
+   (->order-line {:sku      sku
+                  :quantity line-qty})])
 
 ;; Tests
 
 (deftest batches-tests
   (testing "allocating to a batch reduces the available quantity"
     (let [;; Arrange 
-          batch #:Batch{:reference "batch-001"
-                        :sku       "SMALL-TABLE"
-                        :quantity  20
-                        :eta       (now)}
-          line  #:OrderLine{:order-id "order-ref"
-                            :sku      "SMALL-TABLE"
-                            :quantity 2}
+          batch (->batch {:reference "batch-001"
+                          :sku       "SMALL-TABLE"
+                          :quantity  20
+                          :eta       (now)})
+          line  (->order-line {:order-id "order-ref"
+                               :sku      "SMALL-TABLE"
+                               :quantity 2})
          ;; Act
           output (batches/allocate-line batch line)]
      ;; Assert
@@ -63,13 +76,13 @@
       (is output)))
   (testing "cannot allocate if skus do not match"
     (let [;; Arrange
-          batch #:Batch{:reference "batch-001"
-                        :sku       "SMALL-TABLE"
-                        :quantity  20
-                        :eta       (now)}
-          line  #:OrderLine{:order-id "order-ref"
-                            :sku      "LARGE-TABLE"
-                            :quantity 2}
+          batch (->batch {:reference "batch-001"
+                          :sku       "SMALL-TABLE"
+                          :quantity  20
+                          :eta       (now)})
+          line  (->order-line {:order-id "order-ref"
+                               :sku      "LARGE-TABLE"
+                               :quantity 2})
           ;; Act
           output       (batches/can-allocate batch line)]
       ;; Assert
