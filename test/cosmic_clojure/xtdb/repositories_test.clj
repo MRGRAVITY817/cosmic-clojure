27fec542da147e574286582b5a085ee5fc5bfb70
Hoon Wee <mrgravity817@gmail.com>
2024-09-24 23:02:37 +0900

    ch-02: test batch->db-model

---
+++ test/cosmic_clojure/xtdb/repositories_test.clj
@@ -0,0 +1,24 @@
+(ns cosmic-clojure.xtdb.repositories-test
+  (:require [clojure.test :refer [deftest is]]
+            [cosmic-clojure.batch.batches :as batches]
+            [cosmic-clojure.xtdb.repositories :as sut]))
+
+(deftest batch->db-model-test
+  (let [;; Arrange
+        batch  (batches/->batch {:reference "batch-001",
+                                 :sku       "SMALL-TABLE",
+                                 :quantity  10,
+                                 :eta       nil})
+        ;; Act
+        output (sut/batch->db-model batch)]
+    ;; Assert
+    (is (= output
+           {:batch/reference   "batch-001",
+            :batch/sku         "SMALL-TABLE",
+            :batch/purchased-quantity 10,
+            :batch/eta         nil,
+            :batch/allocations #{}}))))
+
+(comment
+  (clojure.test/run-tests)
+  :rcf)
